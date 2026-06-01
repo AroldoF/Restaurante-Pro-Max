5fb24ad7b74c27e1908189f2b3b5e6839eaf0f94
RenataEmy <renata.emy2012@gmail.com>
2026-06-01 20:17:55 -0300

    feat: finally publisher payment

--- Distribuidos/payment-service/app/routes/payments.py
+++ Distribuidos/payment-service/app/routes/payments.py
@@ -1,40 +1,25 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
-from ..repositories.payments import PaymentRepository
-from ..services.payments import PaymentService
-from ..integrations.orders import OrdersIntegrations
-from ..config.database import get_session
+from fastapi import APIRouter, Depends
 from ..schemas.payments import PaymentDetail, PaymentCreate
 from http import HTTPStatus
-from app.config.core import settings
-
+from dependencies.payments import PaymentServiceDep
 
 router = APIRouter(prefix='/payments', tags=['Payments'])
 
-
-# Ajustar rotas
-def get_payment_service(
-    session: Session = Depends(get_session),
-) -> PaymentService:
-    payment_repository = PaymentRepository(session)
-    order_integration = OrdersIntegrations(base_url=settings.ORDER_API_URL)
-    return PaymentService(payment_repository, order_integration)
-
 @router.get("/", response_model=list[PaymentDetail])
-def list_payments(service: PaymentService = Depends(get_payment_service)):
+def list_payments(service: PaymentServiceDep):
     return service.list()
 
 @router.post("/", response_model=PaymentDetail, status_code=HTTPStatus.CREATED)
-def create_payment(data: PaymentCreate, service: PaymentService = Depends(get_payment_service)):
+def create_payment(data: PaymentCreate, service: PaymentServiceDep):
     return service.create(data)
 
 @router.get("/{payment_id}/", response_model=PaymentDetail)
-def get_payment(payment_id: int, service: PaymentService = Depends(get_payment_service)):
+def get_payment(payment_id: int, service: PaymentServiceDep):
     payment = service.get_by_id(payment_id)
     return payment
 
-# @router.post("/{payment_id}/confirm", response_model=PaymentDetail)
-# def confirm_payment(payment_id: int, service: PaymentService = Depends(get_payment_service)):
-#     payment = service.confirm(payment_id)
-#     return payment
+@router.post("/{payment_id}/confirm", response_model=PaymentDetail)
+def confirm_payment(payment_id: int, service: PaymentServiceDep):
+    payment = service.confirm(payment_id)
+    return payment
 
